2d5a3bd7b9465e6c6e2b7e5889bf9b6ffe4baf4b
Kehinde Onifade <onifkay@gmail.com>
2022-11-29 14:06:54 +0100

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,14 @@
 class Post < ApplicationRecord
   after_save :update_posts_counter
-  
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
 
   def recent_five
     comments.order(created_at: :desc).limit(5)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
-
   def recent_three
     posts.order(created_at: :desc).limit(3)
   end
