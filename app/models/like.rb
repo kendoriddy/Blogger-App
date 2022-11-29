8090e33e5cda8aa482830fcbcfe5a7fbba431618
Kehinde Onifade <onifkay@gmail.com>
2022-11-29 12:54:46 +0100

    fix errors with database

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,8 @@
 class Like < ApplicationRecord
-  after_save :update_likes_counter
-
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
-  private
+  after_save :update_likes_counter
 
   def update_likes_counter
     post.increment!(:likes_counter)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', has_many :comments, foreign_key: 'author_id', has_many :likes, foreign_key: 'author_id',
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true,
-  greater_than_or_equal_to: 0 }
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
 
   def recent_three
-    posts.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(13)
   end
 end
