e9f254121ef8d05e95f4472055d289ab2165b70d
Kehinde Onifade <onifkay@gmail.com>
2022-11-30 12:31:23 +0100

    add methods to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+    before_action :set_user, only: [:create]
+    before_action :set_post, only: [:create]
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.post = @post
+        @comment.author = current_user
+        if @comment.save
+            redirect_to user_post_path(@user, @post)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def new
+        @comment = Comment.new
+    end
 end
