e96c81804b19908a0c75f2e9bf15b32311555ba5
Kehinde Onifade <onifkay@gmail.com>
2022-12-09 21:46:55 +0100

    fix linters

--- app/controllers/api/v1/like_controller.rb
+++ app/controllers/api/v1/like_controller.rb
@@ -1,13 +1,13 @@
-class Api::V1::likesController < ApiController
-    def index
-      @post = Post.find(params[:post_id])
-    end
-  
-    def create
-      @post = Post.find(params[:post_id])
-  
-      return unless @post.likes.create(text: params[:like], author: @current_user)
-  
-      render json: { success: 'like added successfully' }
-    end
+class Api::V1::LikesController < ApiController
+  def index
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+
+    return unless @post.likes.create(text: params[:like], author: @current_user)
+
+    render json: { success: 'like added successfully' }
+  end
 end
